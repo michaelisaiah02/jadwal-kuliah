3d9e5dd8b12eeb85265f344a53a3904f0ae3b9d9
michaelisaiah02 <michael.isaiah.02@gmail.com>
2022-10-11 15:17:38 +0700

    jadwal kuliah

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  $("ul li a").on("click", function () {
+    $("ul li a").removeClass("active");
+    $(this).addClass("active");
+  });
+
+  var hari = new Date();
+  if (hari.getDay() === 0) {
+    $("ul li a").removeClass("active");
+    $("ul li a#semua").addClass("active");
+  }
+
+  if ($("ul li a#semua").hasClass("active")) {
+    $("#konten").load("semua.html");
+  }
+  if ($("ul li a#harian").hasClass("active")) {
+    $("#konten").load("harian.html");
+  }
+
+  $("#semua").click(function () {
+    $("#konten").load("semua.html");
+  });
+
+  $("#harian").click(function () {
+    $("#konten").load("harian.html");
+  });
+});
