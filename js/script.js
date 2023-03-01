2cd2f2669ac0fe39f4aa7509670e7504c93829b8
Michael Isaiah <michael.isaiah.02@gmail.com>
2023-03-01 18:41:09 +0700

    Tambah pilih Semester, semester 1, semester 2, semester 5

--- js/script.js
+++ js/script.js
@@ -1,7 +1,37 @@
 $(document).ready(function () {
+  $("#semester1, #semester2, #semester3, #semester4, #semester5, #semester6").on("click", function () {
+    activeSemester = $(this).attr("id");
+    localStorage.setItem("activeSemester", activeSemester);
+    if (activeSemester === "semester1" || activeSemester === "semester2" || activeSemester === "semester3" || activeSemester === "semester4" || activeSemester === "semester5" || activeSemester === "semester6") {
+      $("ul li a.dropdown-item").removeClass("active");
+      $(this).addClass("active");
+    } else if ($(this).attr("id") === "semua") {
+      $("ul li a").removeClass("active");
+      $(this).addClass("active");
+    } else if ($(this).attr("id") === "harian") {
+      $("ul li a").removeClass("active");
+      $(this).addClass("active");
+    }
+  });
   $("ul li a").on("click", function () {
-    $("ul li a").removeClass("active");
-    $(this).addClass("active");
+    if ($(this).attr("id") === "semester") {
+      // jangan lakukan apapun
+    } else if (
+      $(this).attr("id") === "semester1" ||
+      $(this).attr("id") === "semester2" ||
+      $(this).attr("id") === "semester3" ||
+      $(this).attr("id") === "semester4" ||
+      $(this).attr("id") === "semester5" ||
+      $(this).attr("id") === "semester6"
+    ) {
+      // jangan lakukan apapun
+    } else if ($(this).attr("id") === "semua") {
+      $("ul li a.nav-link").removeClass("active");
+      $(this).addClass("active");
+    } else if ($(this).attr("id") === "harian") {
+      $("ul li a.nav-link").removeClass("active");
+      $(this).addClass("active");
+    }
   });
 
   var hari = new Date();
